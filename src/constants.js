ed8a924a7aa4e17ea842064b150c3a1b17f73b27
Tuan Duong <tuan.duong@dynamo.works>
2020-11-29 16:52:26 -0800

    Generate ids for data constants

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,8 @@
-export const CAROUSEL_PANEL_DATA = [1, 2, 3, 4]
+import shortid from "shortid";
+
+export const CAROUSEL_PANEL_DATA = [
+  { id: shortid.generate(), value: 1},
+  { id: shortid.generate(), value: 2},
+  { id: shortid.generate(), value: 3},
+  { id: shortid.generate(), value: 4}
+]
